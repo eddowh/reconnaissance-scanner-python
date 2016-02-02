26ec963d2cf682f22428fdc10b50511b73542eda
Eddo W. Hintoso <eddo.hintoso@gmail.com>
2016-02-02 23:17:21 +0800

    Added basic functions for creating directories and writing into files

---
+++ general.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+
+def create_directory(directory):
+    """
+    Create a folder if not created yet.
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def write_file(path, data):
+    """
+    Write content into file.
+    """
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
