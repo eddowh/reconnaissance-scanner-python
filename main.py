0c3d99b8ceafa45a3492c8b2c168443180cb5994
Eddo W. Hintoso <eddo.hintoso@gmail.com>
2016-02-03 15:37:46 +0700

    Doesn't add one-liner files of url information anymore

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ def main():
 
 def gather_info(name, url):
     robots_txt = get_robots_txt(url)
+
+    # if robots_txt returns None, it can mean quite a few things
+    # i.e. requesting the robots.txt file from reddit in Indonesia
+    # is not possible because of CertificateError - simply put,
+    # Reddit is blocked in Indonesia
+    if not robots_txt:
+        return
+
     domain_name = get_tld(url)
     ip_address = get_ip_address(domain_name)
     nmap = get_nmap('-F', ip_address)
@@ -40,8 +48,6 @@ def create_report(name, url, domain_name, nmap, robots_txt, whois):
     project_dir = "%s/%s" % (ROOT_DIR, name)
     create_dir(project_dir)
 
-    write_file(project_dir + "/" + "full_url.txt", url)
-    write_file(project_dir + "/" + "domain_name.txt", domain_name)
     write_file(project_dir + "/" + "nmap.txt", nmap)
     write_file(project_dir + "/" + "robots.txt", robots_txt)
     write_file(project_dir + "/" + "whois.txt", whois)
